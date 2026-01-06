60355480306a3ae2a1699ffd1c87637be5787655
averyocean65 <averytoad65@gmail.com>
2026-01-07 00:15:16 +0100

    basic library init

---
+++ src/main/java/org/averyocean65/folklore/Library.java
@@ -0,0 +1,11 @@
+package org.averyocean65.folklore;
+
+public final class Library {
+    /**
+     * Function to test if the library works.
+     * @return
+     */
+    public static int Test() {
+        return 400;
+    }
+}
