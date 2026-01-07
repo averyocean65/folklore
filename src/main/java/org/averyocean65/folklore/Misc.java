275d8989a6802063d5da5a50426e120e632073d2
averyocean65 <averytoad65@gmail.com>
2026-01-07 08:49:33 +0100

    add gcd/hcf and lcm

---
+++ src/main/java/org/averyocean65/folklore/Misc.java
@@ -0,0 +1,33 @@
+package org.averyocean65.folklore;
+
+public class Misc {
+    /**
+     * Returns the greatest common divisor of two integer numbers.
+     * This is also known as the HCF (highest common factor).
+     * @param a
+     * @param b
+     * @return the calculated divisor.
+     */
+    public static int gcd(int a, int b) {
+        int c = b;
+        int d = a % b;
+
+        while(d != 0) {
+            int temp = d;
+            d = c % d;
+            c = temp;
+        }
+
+        return c;
+    }
+
+    /**
+     * Calculates the least common multiplier of two numbers.
+     * @param a
+     * @param b
+     * @return the calculated multiplier.
+     */
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+}
