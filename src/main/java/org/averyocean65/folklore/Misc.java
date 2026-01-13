09391c5475e9bf96a5b6fc8c5e62820d1321762b
averyocean65 <averytoad65@gmail.com>
2026-01-13 08:01:47 +0100

    document misc.java more

--- src/main/java/org/averyocean65/folklore/Misc.java
+++ src/main/java/org/averyocean65/folklore/Misc.java
@@ -31,24 +31,49 @@ public class Misc {
         return (a * b) / gcd(a, b);
     }
 
-    public static int factorial(int base) {
+    /**
+     * Returns the factorial form of a number, following the formula: {@code (n - 0) * (n - 1) * (n - 2) * (n - 3) * ...}<br>
+     * Example: factorial(5) = 5! = 5 * 4 * 3 * 2 * 1 = 120
+     * @param n the number to get the factorial of
+     * @return the factorial of n
+     */
+    public static int factorial(int n) {
         int totalValue = 1;
-        for(int i = base; i > 1; i--) {
+        for(int i = n; i > 1; i--) {
             totalValue *= i;
         }
         return totalValue;
     }
 
+    /**
+     * Checks if the difference of two doubles is near zero, therefore checking if they are approximately equal.
+     * @param a the first double
+     * @param b the second double
+     * @return returns whether the numbers are approximately equal or not
+     */
     public static boolean areDoublesEqual(double a, double b) {
         return areDoublesEqual(a, b, true);
     }
 
+    /**
+     * Checks if the difference of two doubles is within a certain error, therefore checking if they are approximately equal.
+     * @param a the first double
+     * @param b the second double
+     * @param extraPrecision toggles whether to use extra precision or not
+     * @return returns whether the numbers are approximately equal or not
+     */
     public static boolean areDoublesEqual(double a, double b, boolean extraPrecision) {
         double error = Math.abs(a - b);
-        double errorMargin = extraPrecision ? 0.00001f : 0.001f;
+        double errorMargin = extraPrecision ? 0.00001 : 0.001;
         return error < errorMargin;
     }
 
+    /**
+     * Takes the nth root of a number.
+     * @param base the number to take the root from.
+     * @param n the degree of the root.
+     * @return the nth root of base.
+     */
     public static double Root(double base, double n) {
         if(areDoublesEqual(n, 1)) {
             return base;
@@ -66,7 +91,13 @@ public class Misc {
         return (double) Math.round(unrounded * 1000f) / 1000f;
     }
 
-    public static double Log(double num, double base) {
-        return (Math.log(num) / Math.log(base));
+    /**
+     * Returns a logarithm with the nth base.
+     * @param num the number to take the logarithm from
+     * @param n the base of the logarithm
+     * @return the logarithm of base n.
+     */
+    public static double Log(double num, double n) {
+        return (Math.log(num) / Math.log(n));
     }
 }
